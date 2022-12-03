9cf8ab512503533512bb3d3411e99ffa5361311e
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-03 20:08:29 +0100

    searchbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import "./App.css";
 import { fetchMeal } from "./Services/api";
+import _debounce from "lodash.debounce";
+import Searchbar from "./Components/Searchbar/Searchbar";
+import { useEffect, useState } from "react";
 
 function App() {
-	const fetchByQuery = async () => {
+	const [query, setQuery] = useState();
+
+	const fetchByQuery = async (query) => {
 		try {
-			const meals = await fetchMeal();
+			const meals = await fetchMeal("name", query);
 			console.log(meals);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
-	fetchByQuery();
+	const fetchRandom = async () => {
+		try {
+			const meal = await fetchMeal("random");
+			console.log(meal);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-	return <></>;
+	const setQueryFromSerchbar = (e) => {
+		setQuery(e.target.value);
+	};
+
+	useEffect(() => {
+		if (query) {
+			fetchByQuery(query);
+		}
+	}, [query]);
+
+	useEffect(() => {
+		fetchRandom();
+	}, []);
+
+	return (
+		<main>
+			<section>
+				<div className="container">
+					<Searchbar onChangeHandler={_debounce(setQueryFromSerchbar, 600)}></Searchbar>
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
--- src/Services/api.js
+++ src/Services/api.js
@@ -2,8 +2,18 @@ import axios from "axios";
 
 const BASE_URL = `https://www.themealdb.com/api/json/v1/1/`;
 
-export const fetchMeal = async (params) => {
-	const response = await axios.get(`${BASE_URL}search.php?s=Panc`);
+export const fetchMeal = async (type, query) => {
+	let fullQuery = "";
+
+	if (type === "name") {
+		fullQuery = `search.php?s=${query}`;
+	}
+
+	if (type === "random") {
+		fullQuery = `random.php`;
+	}
+
+	const response = await axios.get(`${BASE_URL}${fullQuery}`);
 	return response.data.meals;
 };
 
